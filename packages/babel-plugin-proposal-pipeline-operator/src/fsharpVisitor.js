573fe40b228d09be7bfd579a0e3aa959f3da25b3
Thiago Arrais <thiago.arrais@gmail.com>
2019-04-22 21:34:05 -0300

    Extracts arrow function optimization to its own function

--- packages/babel-plugin-proposal-pipeline-operator/src/fsharpVisitor.js
+++ packages/babel-plugin-proposal-pipeline-operator/src/fsharpVisitor.js
@@ -1,54 +1,74 @@
 import { types as t } from "@babel/core";
 
+// tries to optimize sequence expressions in the format
+//   (a = b, ((c) => d + e)(a))
+// to
+//   (a = b, a + e)
+const maybeOptimizePipelineSequence = path => {
+  const [assignNode, callNode] = path.node.expressions;
+  const { left: placeholderNode, right: pipelineLeft } = assignNode;
+  const { callee: calledExpression } = callNode;
+
+  let optimizeArrow =
+    t.isArrowFunctionExpression(calledExpression) &&
+    t.isExpression(calledExpression.body) &&
+    !calledExpression.async &&
+    !calledExpression.generator;
+  let param;
+
+  if (optimizeArrow) {
+    const { params } = calledExpression;
+    if (params.length === 1 && t.isIdentifier(params[0])) {
+      param = params[0];
+    } else if (params.length > 0) {
+      optimizeArrow = false;
+    }
+  } else if (t.isIdentifier(calledExpression, { name: "eval" })) {
+    const evalSequence = t.sequenceExpression([
+      t.numericLiteral(0),
+      calledExpression,
+    ]);
+
+    path.get("expressions.1.callee").replaceWith(evalSequence);
+  }
+
+  if (optimizeArrow && !param) {
+    // Arrow function with 0 arguments
+    path.replaceWith(
+      t.sequenceExpression([pipelineLeft, calledExpression.body]),
+    );
+    return;
+  }
+
+  if (param) {
+    path
+      .get("expressions.1.callee.body")
+      .scope.rename(param.name, placeholderNode.name);
+    path.get("expressions.1").replaceWith(calledExpression.body);
+  }
+};
+
 const fsharpVisitor = {
   BinaryExpression(path) {
     const { scope } = path;
     const { node } = path;
-    const { operator, left } = node;
-    let { right } = node;
+    const { operator, left, right } = node;
     if (operator !== "|>") return;
 
-    let optimizeArrow =
-      t.isArrowFunctionExpression(right) &&
-      t.isExpression(right.body) &&
-      !right.async &&
-      !right.generator;
-    let param;
-
-    if (optimizeArrow) {
-      const { params } = right;
-      if (params.length === 1 && t.isIdentifier(params[0])) {
-        param = params[0];
-      } else if (params.length > 0) {
-        optimizeArrow = false;
-      }
-    } else if (t.isIdentifier(right, { name: "eval" })) {
-      right = t.sequenceExpression([t.numericLiteral(0), right]);
-    }
-
-    if (optimizeArrow && !param) {
-      // Arrow function with 0 arguments
-      path.replaceWith(t.sequenceExpression([left, right.body]));
-      return;
-    }
-
-    const placeholder = scope.generateUidIdentifierBasedOnNode(param || left);
+    const placeholder = scope.generateUidIdentifierBasedOnNode(left);
     scope.push({ id: placeholder });
-    if (param) {
-      path.get("right").scope.rename(param.name, placeholder.name);
-    }
 
     const applied =
       right.type === "AwaitExpression"
         ? t.awaitExpression(t.cloneNode(placeholder))
         : t.callExpression(right, [t.cloneNode(placeholder)]);
-    const call = optimizeArrow ? right.body : applied;
-    path.replaceWith(
-      t.sequenceExpression([
-        t.assignmentExpression("=", t.cloneNode(placeholder), left),
-        call,
-      ]),
-    );
+    const call = applied;
+    const sequence = t.sequenceExpression([
+      t.assignmentExpression("=", t.cloneNode(placeholder), left),
+      call,
+    ]);
+    path.replaceWith(sequence);
+    maybeOptimizePipelineSequence(path);
   },
 };
 
