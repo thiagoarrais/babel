32dad8c5ea2fdf279af6913f2cebccd7980d5a47
Thiago Arrais <thiago.arrais@gmail.com>
2019-05-09 04:02:16 +0000

    Optimize simple function calls

--- packages/babel-plugin-proposal-pipeline-operator/src/buildOptimizedSequenceExpression.js
+++ packages/babel-plugin-proposal-pipeline-operator/src/buildOptimizedSequenceExpression.js
@@ -6,26 +6,26 @@ import { types as t } from "@babel/core";
 //   (a = b, a + e)
 const buildOptimizedSequenceExpression = ({ assign, call, path }) => {
   const { left: placeholderNode, right: pipelineLeft } = assign;
-  const { callee: calledExpression } = call;
+  const { callee: pipelineRight } = call;
 
   let optimizeArrow =
-    t.isArrowFunctionExpression(calledExpression) &&
-    t.isExpression(calledExpression.body) &&
-    !calledExpression.async &&
-    !calledExpression.generator;
+    t.isArrowFunctionExpression(pipelineRight) &&
+    t.isExpression(pipelineRight.body) &&
+    !pipelineRight.async &&
+    !pipelineRight.generator;
   let param;
 
   if (optimizeArrow) {
-    const { params } = calledExpression;
+    const { params } = pipelineRight;
     if (params.length === 1 && t.isIdentifier(params[0])) {
       param = params[0];
     } else if (params.length > 0) {
       optimizeArrow = false;
     }
-  } else if (t.isIdentifier(calledExpression, { name: "eval" })) {
+  } else if (t.isIdentifier(pipelineRight, { name: "eval" })) {
     const evalSequence = t.sequenceExpression([
       t.numericLiteral(0),
-      calledExpression,
+      pipelineRight,
     ]);
 
     call.callee = evalSequence;
@@ -33,11 +33,17 @@ const buildOptimizedSequenceExpression = ({ assign, call, path }) => {
     path.scope.push({ id: placeholderNode });
 
     return t.sequenceExpression([assign, call]);
+  } else if (
+    (t.isIdentifier(pipelineRight) &&
+      path.scope.hasBinding(pipelineRight.name)) ||
+    t.isImmutable(pipelineLeft)
+  ) {
+    return t.callExpression(pipelineRight, [pipelineLeft]);
   }
 
   if (optimizeArrow && !param) {
     // Arrow function with 0 arguments
-    return t.sequenceExpression([pipelineLeft, calledExpression.body]);
+    return t.sequenceExpression([pipelineLeft, pipelineRight.body]);
   }
 
   path.scope.push({ id: placeholderNode });
@@ -45,7 +51,7 @@ const buildOptimizedSequenceExpression = ({ assign, call, path }) => {
   if (param) {
     path.get("right").scope.rename(param.name, placeholderNode.name);
 
-    return t.sequenceExpression([assign, calledExpression.body]);
+    return t.sequenceExpression([assign, pipelineRight.body]);
   }
 
   return t.sequenceExpression([assign, call]);
--- packages/babel-plugin-proposal-pipeline-operator/test/fixtures/minimal/precedence/output.js
+++ packages/babel-plugin-proposal-pipeline-operator/test/fixtures/minimal/precedence/output.js
@@ -2,12 +2,12 @@ var _ref, _ref2, _;
 
 var inc = x => x + 1;
 
-var result = (_ref = 4 || 9, inc(_ref));
+var result = inc(4 || 9);
 expect(result).toBe(5);
 
 var f = x => x + 10;
 
 var h = x => x + 20;
 
-var result2 = (_ref2 = (_ = 10, (f || h)(_)), inc(_ref2));
+var result2 = inc((f || h)(10));
 expect(result2).toBe(21);
