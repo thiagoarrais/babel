b061ef41a5a1f74d545aa23fa0adaacdb891f117
Thiago Arrais <thiago.arrais@gmail.com>
2019-05-24 16:06:06 +0000

    Better docs

--- packages/babel-plugin-proposal-pipeline-operator/src/transformPipelineExpression.js
+++ packages/babel-plugin-proposal-pipeline-operator/src/transformPipelineExpression.js
@@ -1,40 +1,36 @@
 import { types as t } from "@babel/core";
 
-// cases:
-// 1: a |> (x) => (x + x)     -->     (_ref = a, _ref + _ref)
-// 2: a |> () => b            -->     (a, b)
-// 3: a |> eval               -->     (_ref = a, (0, eval)(_ref))
-// 4: 0 |> b                  -->     b(0)
-// 5: a |> b                  -->     (_ref = a, b(_ref))
+// Transpiles pipeline expressions
+//  path: path for pipeline BinaryExpression
+//  makeCall [(callee, argument) => node]:
+//    proposal-dependent function that applies the result from evaluating
+//    the left side to the expression the right side
 const transformPipelineExpression = (path, makeCall) => {
   const { scope, node } = path;
   const { left, right } = node;
 
-  const optimizeArrow =
-    t.isArrowFunctionExpression(right) &&
-    t.isExpression(right.body) &&
-    !right.async &&
-    !right.generator;
-  let param;
-
   const placeholder = scope.generateUidIdentifierBasedOnNode(left);
   const assign = t.assignmentExpression("=", t.cloneNode(placeholder), left);
 
-  if (optimizeArrow) {
-    const { params } = right;
-    if (params.length === 0) {
-      // Arrow function with 0 arguments
-      return t.sequenceExpression([left, right.body]);
-    } else if (params.length === 1 && t.isIdentifier(params[0])) {
-      param = params[0];
+  if (isOptimizableArrow(right) && right.params.length === 0) {
+    // Arrow function with 0 arguments
+    // a |> () => b            -->     (a, b)
+    return t.sequenceExpression([left, right.body]);
+  } else if (
+    isOptimizableArrow(right) &&
+    right.params.length === 1 &&
+    t.isIdentifier(right.params[0])
+  ) {
+    // a |> (x) => (x + x)     -->     (_ref = a, _ref + _ref)
+    const param = right.params[0];
 
-      scope.push({ id: placeholder });
+    scope.push({ id: placeholder });
 
-      path.get("right").scope.rename(param.name, placeholder.name);
+    path.get("right").scope.rename(param.name, placeholder.name);
 
-      return t.sequenceExpression([assign, right.body]);
-    }
+    return t.sequenceExpression([assign, right.body]);
   } else if (t.isIdentifier(right, { name: "eval" })) {
+    // a |> eval               -->     (_ref = a, (0, eval)(_ref))
     const evalSequence = t.sequenceExpression([t.numericLiteral(0), right]);
 
     const call = makeCall(evalSequence, t.cloneNode(placeholder));
@@ -46,13 +42,21 @@ const transformPipelineExpression = (path, makeCall) => {
     (t.isIdentifier(right) && scope.hasBinding(right.name)) ||
     t.isImmutable(left)
   ) {
+    // 0 |> b                  -->     b(0)
     return t.callExpression(right, [left]);
+  } else {
+    // a |> b                  -->     (_ref = a, b(_ref))
+    const call = makeCall(right, t.cloneNode(placeholder));
+    scope.push({ id: placeholder });
+
+    return t.sequenceExpression([assign, call]);
   }
-
-  const call = makeCall(right, t.cloneNode(placeholder));
-  scope.push({ id: placeholder });
-
-  return t.sequenceExpression([assign, call]);
 };
+
+const isOptimizableArrow = expr =>
+  t.isArrowFunctionExpression(expr) &&
+  t.isExpression(expr.body) &&
+  !expr.async &&
+  !expr.generator;
 
 export default transformPipelineExpression;
